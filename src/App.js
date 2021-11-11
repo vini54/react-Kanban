c841efff11df4668d31bed3867fd3ad73928a646
vini54 <vinioli544@gmail.com>
2021-11-11 17:34:54 -0300

    updating

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
 import TaskList from './components/TaskList/TaskList';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 
 let idAcc = 0
 const generateId = () => {
